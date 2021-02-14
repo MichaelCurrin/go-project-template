957108c3b2015bc1917df7e44e42150dedcc87a2
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-14 11:19:54 +0200

    feat: Setup custom greeting and CLI fields

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	(&cli.App{}).Run(os.Args)
+	app := &cli.App{
+		Name:        "My App",
+		HelpName:    "myapp",
+		Usage:       "Display a friendly greeting",
+		Version:     "v0.2.0",
+		Description: "A Hello World greeting app to get you started with a new Go project",
+
+		// Commands:    []*cli.Command{},
+		// Flags:       []cli.Flag{},
+
+		Action: func(c *cli.Context) error {
+			name := "World"
+			if c.NArg() > 0 {
+				name = c.Args().Get(0)
+			}
+			fmt.Printf("Hello, %v!\n", name)
+
+			return nil
+		},
+		Compiled: time.Time{},
+		Authors: []*cli.Author{
+			{
+				Name:  "MichaelCurrin",
+				Email: "",
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
